00f05d830fe698e5adac78eeed6b45fa165f5e62
Agron Gojcaj <agrongojcaj6@gmail.com>
2025-02-12 01:52:24 -0500

    Adjusted expertise paragraphs

--- src/components/Expertise.tsx
+++ src/components/Expertise.tsx
@@ -31,6 +31,7 @@ const labelsSecond = [
     "Google Colab",
     "Keras",
     "R",
+    "NLTK",
 ];
 
 function Expertise() {
@@ -41,8 +42,8 @@ function Expertise() {
             <div className="skills-grid">
                 <div className="skill">
                     <FontAwesomeIcon icon={faReact} size="3x"/>
-                    <h3>Full Stack Web Development</h3>
-                    <p>I have built a diverse array of web applications from scratch using modern technologies such as React and Flask. I have a strong proficiency in the SDLC process and frontend + backend development.</p>
+                    <h3>Full Stack Development</h3>
+                    <p>I have built a diverse array of web applications using modern technologies such as React and Flask. I have a strong proficiency in the SDLC process and frontend + backend development.</p>
                     <div className="flex-chips">
                         <span className="chip-title">Technologies:</span>
                         {labelsFirst.map((label, index) => (
@@ -54,7 +55,7 @@ function Expertise() {
                 <div className="skill">
                     <FontAwesomeIcon icon={faPython} size="3x"/>
                     <h3>AI & Machine Learning</h3>
-                    <p>Stay relevant in the market by leveraging the latest AI models in your projects. I have professional experience building enterprise grade GenAI-enabled solutions to empower intelligent decision making.</p>
+                    <p>I have experience building machine learning and deep learning models that aim to solve real-world problems.</p>
                     <div className="flex-chips">
                         <span className="chip-title">Technologies:</span>
                         {labelsSecond.map((label, index) => (
