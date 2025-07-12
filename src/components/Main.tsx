a76a8f41c7fc702c21cbad446212856923125087
Agron Gojcaj <agrongojcaj6@gmail.com>
2025-07-12 02:26:24 -0400

    Headshot image now loads

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import '../assets/styles/Main.scss';
+import headshot from "../assets/images/Updated_Headshot.png";
 
 function Main() {
 
@@ -9,7 +10,7 @@ function Main() {
     <div className="container">
       <div className="about-section">
         <div className="image-wrapper">
-          <img src="https://media.licdn.com/dms/image/v2/D5603AQECBBfoRmUOXg/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1710964655314?e=1744848000&v=beta&t=pqG37oE12BQrrFRGA_dmHCVYV5DLQaQ-D5vb9k8zEP0" alt="Avatar" />
+          <img src={headshot} alt="Avatar" />
         </div>
         <div className="content">
           <div className="social_icons">
